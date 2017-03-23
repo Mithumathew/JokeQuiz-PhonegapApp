6bda666c94bec808583c912f3c7cd56b1587fffb
Elizabeth <lizcovenant7@gmail.com>
2017-03-23 00:05:25 -0400

    jokequiz

--- www/js/index.js
+++ www/js/index.js
@@ -3,50 +3,28 @@
     var data = {
         quizContent: [
             {
-                question: "Who is the founder of Quora?",
-                answer1: "Dick Costolo",
-                answer2: "Mark Zuckerberg",
-                answer3: "Adam D'Angelo",
-                fact:"The correct answer is Adam D'Angelo",
-                correctAnswer: 3
+                question: "What does a nosey pepper do?",
+                fact:"Gets Jalapeno business",
             },
             {
-                question: "Where is the Microsoft Corporation headquarters located?",
-                answer1: "Mountain View",
-                answer2: "Redmond",
-                answer3: "Cupertino",
-                fact:"The correct answer is Redmond",
-                
-                correctAnswer: 2
+                question: "Whats the difference between a guitar and a fish?",
+                fact:"You can't tuna fish",
             },
             {
-                question:  "Which billion-dollar company purchased Instagram ?",
-                answer1: "Amazon",
-                answer2: "Facebook",
-                answer3: "Twitter",
-                fact:"The correct answer is Facebook",
-                correctAnswer: 2
+                question:  "What do you get from a pampered cow?",
+                fact:"Spoiled Milk",
             },
             {
-                question: "Which information-technology company was founded on April Fool's Day?",
-                answer1: "Microsoft",
-                answer2: "Amazon",
-                answer3: "Apple",
-                fact:"The correct answer is Apple",
-                correctAnswer: 3
+                question: "What do you call security guards working outside Samsung shops?",
+                fact:"Guardians of the Galaxy",
             },
             {
-                question  : "Who is the present CEO of Yahoo!?",
-                answer1  : "Marissa Mayer",
-                answer2  : "Tim Cook",
-                answer3  : "Sheryl Sandberg",
-                fact:"The correct answer is Marissa Mayer",
-                correctAnswer: 1
+                question  : "Where did the computer go to dance?",
+                fact:"To a Disc-o",
                   
                     
             }
         ],
-        points: 0
     };
     
     var display = {
@@ -55,12 +33,6 @@
         // Updates DOM on start/restart of the quiz
         mainPage: function() {
             var newEl = '<div class="quest-number"><p id="questNumber"></p></div><h1 id="questionDisplay" class="h3"></h1>';
-                newEl += '<ul><li><label><input type="radio" name="answers" id="input1" value="1"><span class="outer"><span class="inner"></span></span><div id="answerDisplay1"></div></label></li>';
-                newEl += '<li><label><input type="radio" name="answers" id="input2" value="2"><span class="outer"><span class="inner"></span></span><div id="answerDisplay2"></div></label></li>';
-                newEl += '<li><label><input type="radio" name="answers" id="input3" value="3"><span class="outer"><span class="inner"></span></span><div id="answerDisplay3"></div></label></li></ul>';
-                
-                newEl += '<div class="points-wrap"><p id="currentPoints"></p></div>';
-
             this.getApp.innerHTML = newEl;
         },
 
@@ -68,20 +40,7 @@
         updateMainPage: function() {
             var getQuestNumber = document.getElementById('questNumber'),
                 getQuestion = document.getElementById('questionDisplay'),
-                getAnswer1 = document.getElementById('answerDisplay1'),
-                getAnswer2 = document.getElementById('answerDisplay2'),
-                getAnswer3 = document.getElementById('answerDisplay3'),
-               
-                getCurrentPoints = document.getElementById('currentPoints'),
-                sumOfQuestions = data.quizContent.length;
                 
-            getQuestNumber.innerHTML = control.count + 1 + '/' + sumOfQuestions;
-            getQuestion.innerHTML = data.quizContent[control.count].question;
-            getAnswer1.innerHTML = data.quizContent[control.count].answer1;
-            getAnswer2.innerHTML = data.quizContent[control.count].answer2;
-            getAnswer3.innerHTML = data.quizContent[control.count].answer3;
-         
-            getCurrentPoints.innerHTML = 'Points:' + ' ' + data.points;
             this.newElement('button', 'submit', 'Submit Answer');
         },
         addAnswer: function(showMessage) {
@@ -153,33 +112,9 @@
         * Verifies correct/incorrect answer
         * Directs quiz to the next question or to the final page
         */
-        checkAnswer: function(event) {
-            var radioButtons = document.getElementsByName('answers'),
-                allRadioButtons = radioButtons.length,
-                isChecked = false,
-                checkedRadio,
-                clickedEl = event.target,
-                clickedElParent = clickedEl.parentNode,
-                i;
-
-            for (i = 0; i < allRadioButtons; i++) {
-                if (radioButtons[i].checked) {
-                    isChecked = true;
-                    checkedRadio = +radioButtons[i].value;
-                }
-            }
-
-            if (isChecked === false) {
-                alert('Please choose the answer!');
-            } else {
-                clickedElParent.removeChild(clickedEl);
-                if (checkedRadio === data.quizContent[control.count].correctAnswer) {
-                    display.addAnswer('correct');
-                    data.points++;
-                } else {
-                    display.addAnswer();
-                }
-
+       
+        
+             checkAnswer: function(event){
                 var nextQuestion = document.getElementById('nextQuest'),
                     result = document.getElementById('result');
 
@@ -201,9 +136,5 @@
         },
 
         // Used for incrementing/looping through the quiz questions and answers
-        count: 0
-    };
-
-    control.init();
-
-})();
+        
+    });

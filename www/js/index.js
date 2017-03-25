fa3c36003e75689d6f492038b07deb8d870d7918
Elizabeth <lizcovenant7@gmail.com>
2017-03-25 10:44:24 -0400

    Jokequizapp

--- www/js/index.js
+++ www/js/index.js
@@ -4,27 +4,46 @@
         quizContent: [
             {
                 question: "What does a nosey pepper do?",
-                fact:"Gets Jalapeno business",
+                answer1: "Sneeze",
+                answer2: "Annoys the pepper family",
+                answer3: "Gets Ja-la-peno business",
+                fact:"Gets Ja-la-peno business",
+                correctAnswer: 3
             },
             {
                 question: "Whats the difference between a guitar and a fish?",
+                answer1: "You can't tuna fish",
+                answer2: "Fish smells",
+                answer3: "Fish don't get played",
                 fact:"You can't tuna fish",
+                correctAnswer: 1
             },
             {
                 question:  "What do you get from a pampered cow?",
+                answer1: "Milk Shake",
+                answer2: "Spoiled Milk",
+                answer3: "Moo-ouse",
                 fact:"Spoiled Milk",
+                correctAnswer: 2
             },
             {
                 question: "What do you call security guards working outside Samsung shops?",
+                answer1: "sleeping guards",
+                answer2: "Guardians of the Galaxy",
+                answer3: "Guards on duty",
                 fact:"Guardians of the Galaxy",
+                correctAnswer: 2
             },
             {
                 question  : "Where did the computer go to dance?",
+                answer1: "Software club",
+                answer2: "Silicon Valley",
+                answer3: "To a Disc-o",
                 fact:"To a Disc-o",
-                  
-                    
+                correctAnswer: 3
             }
         ],
+        points: 0
     };
     
     var display = {
@@ -33,6 +52,12 @@
         // Updates DOM on start/restart of the quiz
         mainPage: function() {
             var newEl = '<div class="quest-number"><p id="questNumber"></p></div><h1 id="questionDisplay" class="h3"></h1>';
+                newEl += '<ul><li><label><input type="radio" name="answers" id="input1" value="1"><span class="outer"><span class="inner"></span></span><div id="answerDisplay1"></div></label></li>';
+                newEl += '<li><label><input type="radio" name="answers" id="input2" value="2"><span class="outer"><span class="inner"></span></span><div id="answerDisplay2"></div></label></li>';
+                newEl += '<li><label><input type="radio" name="answers" id="input3" value="3"><span class="outer"><span class="inner"></span></span><div id="answerDisplay3"></div></label></li></ul>';
+
+                newEl += '<div class="points-wrap"><p id="currentPoints"></p></div>';
+
             this.getApp.innerHTML = newEl;
         },
 
@@ -40,21 +65,34 @@
         updateMainPage: function() {
             var getQuestNumber = document.getElementById('questNumber'),
                 getQuestion = document.getElementById('questionDisplay'),
+                getAnswer1 = document.getElementById('answerDisplay1'),
+                getAnswer2 = document.getElementById('answerDisplay2'),
+                getAnswer3 = document.getElementById('answerDisplay3'),
+
+                getCurrentPoints = document.getElementById('currentPoints'),
+                sumOfQuestions = data.quizContent.length;
                 
+            getQuestNumber.innerHTML = control.count + 1 + '/' + sumOfQuestions;
+            getQuestion.innerHTML = data.quizContent[control.count].question;
+            getAnswer1.innerHTML = data.quizContent[control.count].answer1;
+            getAnswer2.innerHTML = data.quizContent[control.count].answer2;
+            getAnswer3.innerHTML = data.quizContent[control.count].answer3;
+
+            getCurrentPoints.innerHTML = 'Points:' + ' ' + data.points;
             this.newElement('button', 'submit', 'Submit Answer');
         },
         addAnswer: function(showMessage) {
             var sumOfQuestions = data.quizContent.length;
 
             if(showMessage === 'correct') {
-                this.newElement('p', 'showAnswer', 'Correct Answer!');
+                this.newElement('p', 'showAnswer', 'You got that riii...gghhht!HAHA');
             } else {
                 
                 var x=data.quizContent[control.count].fact
               
                 
                 
-                this.newElement('p', 'showAnswer', 'Incorrect !'+x );
+                this.newElement('p', 'showAnswer', 'Awww ! Sorry it is '+x+' .HAHA !' );
             }
 
             if (control.count < sumOfQuestions - 1) {
@@ -112,9 +150,33 @@
         * Verifies correct/incorrect answer
         * Directs quiz to the next question or to the final page
         */
-       
-        
-             checkAnswer: function(event){
+        checkAnswer: function(event) {
+            var radioButtons = document.getElementsByName('answers'),
+                allRadioButtons = radioButtons.length,
+                isChecked = false,
+                checkedRadio,
+                clickedEl = event.target,
+                clickedElParent = clickedEl.parentNode,
+                i;
+
+            for (i = 0; i < allRadioButtons; i++) {
+                if (radioButtons[i].checked) {
+                    isChecked = true;
+                    checkedRadio = +radioButtons[i].value;
+                }
+            }
+
+            if (isChecked === false) {
+                alert('Please choose the answer!');
+            } else {
+                clickedElParent.removeChild(clickedEl);
+                if (checkedRadio === data.quizContent[control.count].correctAnswer) {
+                    display.addAnswer('correct');
+                    data.points++;
+                } else {
+                    display.addAnswer();
+                }
+
                 var nextQuestion = document.getElementById('nextQuest'),
                     result = document.getElementById('result');
 
@@ -136,5 +198,9 @@
         },
 
         // Used for incrementing/looping through the quiz questions and answers
-        
-    });
+        count: 0
+    };
+
+    control.init();
+
+})();
